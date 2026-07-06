e649c8a4c98e506dd2030a5bf1e7c9e1dc7e44e8
Andrew Lai <andrewjameslai@gmail.com>
2026-07-06 21:45:20 +0000

    Undoing changes to get redeploy-site.sh to work on port 5000

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -15,12 +15,11 @@ cd $PROJECT_DIR || { echo "Directory $PROJECT_DIR not found"; exit 1; }
 git fetch && git reset origin/main --hard
 
 # 4. Enter Python venv and install dependencies
-source $VENV_PATH/bin/activate
+source VENV_PATH/bin/activate
 pip install -r requirements.txt
 
 # 5. Start new detached tmux session
-tmux new-session -d -s flask-server 'source '$VENV_PATH'/bin/activate && export FLASK_APP=
-__init.py__ && flask run --host=0.0.0.0 --port=80'
+tmux new-session -d -s flask-server 'source VENV_PATH/bin/activate && flask run --host=0.0.0.0 --port=80'
 
 echo "Deployment complete!"
 echo "Flask server running in tmux session: flask-server"
